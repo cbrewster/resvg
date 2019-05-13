67a87d17bfba23a3a995a5a53acefd9c4f2f4093
Connor Brewster <connor.brewster@eagles.oc.edu>
2019-05-12 18:26:15 -0600

    Implement OutputImage for Image

--- src/backend_piet/mod.rs
+++ src/backend_piet/mod.rs
@@ -1,12 +1,22 @@
 use crate::prelude::*;
 
+use piet_common::{BitmapTarget, Device};
+use piet::ImageFormat;
+
 /// Piet backend
 #[derive(Clone, Copy)]
 pub struct Backend;
 
+pub struct Image {
+    pixels: Vec<u8>,
+    width: u32,
+    height: u32
+}
+
 impl Render for Backend {
     fn render_to_image(&self, tree: &usvg::Tree, opt: &Options) -> Option<Box<OutputImage>> {
-        None
+        let img = render_to_image(tree, opt)?;
+        Some(Box::new(img))
     }
 
     fn render_node_to_image(&self, node: &usvg::Node, opt: &Options) -> Option<Box<OutputImage>> {
@@ -18,3 +28,22 @@ impl Render for Backend {
     }
 }
 
+impl OutputImage for Image {
+    fn save(&self, path: &::std::path::Path) -> bool {
+        image::save_buffer(
+            path,
+            &self.pixels,
+            self.width as u32,
+            self.height as u32,
+            image::ColorType::RGBA(8),
+        ).is_ok()
+    }
+}
+
+/// Renders SVG to image
+pub fn render_to_image(
+    tree: &usvg::Tree,
+    opt: &Options
+) -> Option<Image> {
+    None
+}
